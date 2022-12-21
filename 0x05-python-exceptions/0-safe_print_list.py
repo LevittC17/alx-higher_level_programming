aed16f0d7158026bafa34553775af8ed556825c4
LevittC17 <LeewednesdayC17@protonmail.com>
2022-12-21 14:24:29 +0000

    updated task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
+# 0-safe_print_list.py
 
-def list_division(my_list_1, my_list_2, list_length):
-    """Divides two lists element by element.
 
+def safe_print_list(my_list=[], x=0):
+    """Print x elememts of a list.
     Args:
-        my_list_1 (list): The first list.
-        my_list_2 (list): The second list.
-        list_length (int): The number of elements to divide.
-
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
     Returns:
-        A new list of length list_length containing all the divisions.
+        The number of elements printed.
     """
-    new_list = []
-    for i in range(0, list_length):
+    ret = 0
+    for i in range(x):
         try:
-            div = my_list_1[i] / my_list_2[i]
-        except TypeError:
-            print("wrong type")
-            div = 0
-        except ZeroDivisionError:
-            print("division by 0")
-            div = 0
+            print("{}".format(my_list[i]), end="")
+            ret += 1
         except IndexError:
-            print("out of range")
-            div = 0
-        finally:
-            new_list.append(div)
-    return (new_list)
-
+            break
+    print("")
+    return (ret)
