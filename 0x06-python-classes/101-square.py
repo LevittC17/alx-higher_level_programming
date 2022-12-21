55bfb42fa7baff86017be5bcde9db763ffe0d5ef
LevittC17 <LeewednesdayC17@protonmail.com>
2022-12-21 19:31:43 +0000

    updated advanced task 101

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,6 +1,5 @@
-:wq
 #!/usr/bin/python3
-
+# 101-square.py
 """Define a class Square."""
 
 
@@ -9,7 +8,6 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
-
         Args:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
