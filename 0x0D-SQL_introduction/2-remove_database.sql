c79029b67cb3207eaa00ee5535bd43e8d708e19a
LevittC17 <LeewednesdayC17@protonmail.com>
2023-02-15 07:25:46 +0000

    delete the database, whether it exists or not

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- delete the database hbtn_0c_0 in the MySQL server
 -- if the database does not exist, the script should not fail
-DELETE DATABASE IF NOT EXISTS hbtn_0c_0
+DELETE DATABASE IF EXISTS hbtn_0c_0
