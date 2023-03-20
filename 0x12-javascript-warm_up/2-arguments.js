611ed323c317d2c1a7ef1a49f508dfe51cec64c9
LevittC17 <LeewednesdayC17@protonmail.com>
2023-03-20 11:07:01 +0000

    Write a script that prints a message depending of the number of arguments passed in Javascript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function printMessage(){
+    const noArgv = arguments.length;
+    
+    if (noArgv === 0){
+        console.log("No argument");
+    }
+    else if (noArgv == 1){
+        console.log("Argument found");
+    }
+    else
+        console.log(${noArgv}, arguments);
+}
---
+++ 0x12-javascript-warm_up/let.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var myVar = "Nick";
+    if (true){
+        var myVar = "John";
+        console.log(myVar);
+    }
+    console.log(myVar);
+}
