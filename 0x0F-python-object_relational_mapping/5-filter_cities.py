b2f5d523081d864222849aaa946b871b2b3d4ec1
LevittC17 <LeewednesdayC17@protonmail.com>
2023-03-24 12:00:34 +0000

    all cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT cities.name\
@@ -17,7 +17,7 @@ if __name__ == '__main__':
                 ON states.id = cities.state_id\
                 WHERE states.name = %s\
                 ORDER BY cities.id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
-    print(", ".join([row[0] for row in rows]))
+    data = cur.fetchall()
+    print(", ".join([row[0] for row in data]))
     cur.close()
     db.close()
