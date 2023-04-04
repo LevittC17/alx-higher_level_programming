73b13ee5b7084674b5bdd380554e6d689ade400c
LevittC17 <LeewednesdayC17@protonmail.com>
2023-04-04 13:59:19 +0000

    response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,16 +5,10 @@ Get the URL from the
 command-line argument
 """
 import urllib.request
-import sys
+from sys import argv
 
-url = sys.argv[1]
 
-# Send a request to the URL
-req = urllib.request.Request(url)
-
-# Open the connection and send the request
-with urllib.request.urlopen(req) as response:
-    # Get the value of the X-Request-Id header
-    x_request_id = response.headers.get('X-Request-Id')
-    # Display the value of the X-Request-Id header
-    print(x_request_id)
+if __name__ == "__main__":
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as response:
+        print(response.getheader('X-Request-Id'))
