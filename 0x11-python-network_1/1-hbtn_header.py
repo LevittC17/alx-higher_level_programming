95d88111ff45c935c9f757577b1b503d34b266bf
LevittC17 <LeewednesdayC17@protonmail.com>
2023-04-04 13:54:44 +0000

    response header value

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+import urllib.request
+import sys
+
+# Get the URL from the command-line arguments
+url = sys.argv[1]
+
+# Send a request to the URL
+req = urllib.request.Request(url)
+
+# Open the connection and send the request
+with urllib.request.urlopen(req) as response:
+    # Get the value of the X-Request-Id header
+    x_request_id = response.headers.get('X-Request-Id')
+    # Display the value of the X-Request-Id header
+    print(x_request_id)
