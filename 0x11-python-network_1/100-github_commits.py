15da218167c2bd0eb1ed916401b8f6abc147e701
LevittC17 <LeewednesdayC17@protonmail.com>
2023-04-04 14:34:38 +0000

    take an interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+List 10 latest commits
+argument1: repository name
+argument2: owner name
+packages: requests, sys
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[2], argv[1]))
+    commits = r.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
