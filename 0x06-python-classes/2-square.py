db14a619d39f0c48347edb1218990af1522212ee
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-08 13:05:49 +0000

    Changing the elif statement to 2 if's

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,9 +12,7 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-       if size.isdigit():
-           self.__size = size
-       else:
-           TypeError "size must be an integer"
-       if size < 0
-           ValueError "size must be >= 0" 
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
