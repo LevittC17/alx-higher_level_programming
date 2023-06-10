ff92f9a05b0f10a1a7ae9e874b9d28f26165431a
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-10 09:02:14 +0000

    Created a function that retrieves an element from a list like in C

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -8,7 +8,7 @@ If element is out of range (greater than the length of the list)
 
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
     else:
         return my_list[idx]
