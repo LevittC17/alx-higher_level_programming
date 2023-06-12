b68b7bfd9934cd0e40719708f3c6c9c831f47266
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-12 19:13:28 +0000

    script to print all arguments passed to it in the format 'is'

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+/**
+ * Script that prints 2 arguments passed to it
+ * Format: "is"
+ * Example: ./4-concat.js c cool
+ * Output: c is cool
+*/
+
+const args = process.argv.slice(2)
+
+if (args.length >= 2) {
+  console.log(`${args[0]} is ${args[1]}`);
+} else {
+  console.log('undefined is undefined');
+}
