5c684ef56d290cf6b75fb2d4cf3e9baf72a86b71
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-13 09:47:46 +0000

    print 3 lines from an array using a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/**
+ * Print 3 lines from an array using a loop
+*/
+
+const myLangs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+myLangs.forEach(lang => {
+  console.log(lang);
+})
