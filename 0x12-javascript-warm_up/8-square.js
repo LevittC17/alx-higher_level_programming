ef5a7fec52e77d3f36085586c399a7a3b6c483c8
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-13 11:53:37 +0000

    A script that prints a square -> rows vs columns kind

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+/**
+ * A script that prints a square
+ * Use the character 'X' to print the square
+ * Kinda like a matrix -> rows vs columns
+*/
+
+const args = process.argv.slice(2);
+const size = parseInt(args[0]);
+
+// check if size is a number
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      process.stdout.write('X');
+    }
+    console.log('X');
+  }
+}
