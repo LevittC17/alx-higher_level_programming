18f7e82040d1edae6ddde9cb321d2f7ce54f079a
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-14 23:27:09 +0000

    class Square that inherits from class Rectangle in 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -5,12 +5,10 @@
  * inherits from the class Rectangle in 4-rectangle.js
 */
 
-const Rectangle = require('./4-rectangle.js');
+const Rectangle = require('./4-rectangle');
 
-class Square extends Rectangle {
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-}
-
-module.exports = Square;
+};
