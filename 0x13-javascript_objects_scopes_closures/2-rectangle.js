d73db13c4eaae4a360363208134b5635859ad5d2
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-14 13:41:02 +0000

    class Rectangle - checking it width and height are equal ti zero or not a positive integer

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/**
+ * class Rectangle that defines a rectangle
+ * check if `w` and `h` are eqaul to 0 or not a positive integer
+*/
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 & h < 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
