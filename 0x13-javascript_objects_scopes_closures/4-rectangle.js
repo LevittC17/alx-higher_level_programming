8ecdb00aa153a78a58e575d1901cadcd8cd47d75
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-14 21:45:55 +0000

    class Rectangle, print with 'X', rotate width and height, double width and height by 2

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+/**
+ * class Rectangle that defines a rectangle
+ * instance method print() - print rectangle with 'X' character
+ * instance method rotate() - swap width `w` and height `h`
+ * instance method double() - multiply each width and height by 2
+*/
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    // check if w, h = 0 or < 0
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+
+    this.double = function () {
+      this.width *= 2;
+      this.height *= 2;
+    };
+
+    this.rotate = function () {
+      const temp = this.width;
+      this.width = this.height;
+      this.height = temp;
+    };
+
+    this.print = function () {
+      // Iterate through width and height
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += 'X';
+        }
+        console.log(row);
+      }
+    };
+  }
+};
