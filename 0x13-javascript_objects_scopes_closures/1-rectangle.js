6635f653072764bfa723563e50bfab36668a130d
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-14 12:04:41 +0000

    class Rectangle that defines a rectangle with instance attributes width and height

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/**
+ * class Rectangle that defines a rectangle
+ * Constructor takes 2 arguments, w, h
+ * Instance attribute width = w, height = h
+*/
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
