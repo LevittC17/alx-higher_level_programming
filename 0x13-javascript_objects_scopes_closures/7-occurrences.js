e41c279cf7300cadb30b7813894a17c16d403fc2
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-15 00:05:58 +0000

    function to return the number of occurrences in a list

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/**
+ * Function returning the number of occurences in a list
+ * Prototype: exports.nbOccurences = function (list, searchElement)
+*/
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] == searchElement) {
+      count ++;
+    }
+  }
+  return count;
+}
