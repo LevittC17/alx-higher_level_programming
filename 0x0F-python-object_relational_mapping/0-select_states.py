97a11944b01083c931df1ea5ee781943245e01c5
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-16 20:49:15 +0000

    pycodestyle fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,6 +11,6 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
-                       user=sys.argv[1], passwd=sys.argv[2],
-                       db=sys.argv[3], charset="utf8")
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
     cur = db.cursor()
