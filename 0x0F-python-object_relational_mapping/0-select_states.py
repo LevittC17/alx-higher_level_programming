61f8ff39e6f03511196935cba769a21048cea9c2
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-16 20:28:24 +0000

    checked indentation as per pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,14 +6,14 @@ Arguments: mysql username, mysql password database name
 """
 
 
-from MySQLdb import _mysql
+import MySQLdb
 import sys
 
 if __name__ == "__main__":
     """lists all
     states from the database hbtn_0e_0_usa"""
 
-    db = -_mysql.connect(host="localhost", port=3306,
-                        user=sys.argv[1], passwd=sys.argv[2],
-                        db=sys.argv[3], charset="utf8")
+    db = mysql.connect(host="localhost", port=3306,
+                       user=sys.argv[1], passwd=sys.argv[2],
+                       db=sys.argv[3], charset="utf8")
     cur = db.cursor()
