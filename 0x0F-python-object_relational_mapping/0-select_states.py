721d386fb662bfd7e4d9bec52aa353fd6ef18e2f
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-16 12:22:11 +0000

    a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+A script that lists all states from the database hbtn_0e_0_usa
+Arguments: mysql username, mysql password database name
+"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """lists all
+    states from the database hbtn_0e_0_usa"""
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
+    cur = db.cursor()
