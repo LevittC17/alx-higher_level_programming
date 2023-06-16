3d42254bdfbc08729d9d9e3c26084bc206409be4
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-16 20:31:07 +0000

    removed comments inside __main__

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,9 +10,6 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    """lists all
-    states from the database hbtn_0e_0_usa"""
-
     db = mysql.connect(host="localhost", port=3306,
                        user=sys.argv[1], passwd=sys.argv[2],
                        db=sys.argv[3], charset="utf8")
