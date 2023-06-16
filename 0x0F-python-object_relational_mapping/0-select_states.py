ac4d70a980f93df85e3b50a8c8b0b72f78380b9c
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-16 20:47:06 +0000

    using MySQLdb.connect

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = mysql.connect(host="localhost", port=3306,
+    db = MySQLdb.connect(host="localhost", port=3306,
                        user=sys.argv[1], passwd=sys.argv[2],
                        db=sys.argv[3], charset="utf8")
     cur = db.cursor()
