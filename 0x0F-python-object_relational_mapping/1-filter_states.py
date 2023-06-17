5b43179e541ff2044d185ad10199e2198d162008
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-17 20:29:11 +0000

    added the cursor.close() and db.close() functions

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,6 @@ Script that lists all state with a name starting with N
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
@@ -16,6 +15,8 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE name
                 LIKE BINARY 'N%' ORDER BY states.id ASC""")
-    rows = cur.fetchall()
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
+
+    cur.close()
+    db.close()
