a742d38b28e6f545b4d44ae951af05745433d321
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-17 08:05:36 +0000

    storing results in ascending order

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,3 +14,6 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], charset="utf8")
     cur = db.cursor()
+
+    query = "SELECT * FROM states ORDERED BY states.id ASC"
+    cur.execute(query)
