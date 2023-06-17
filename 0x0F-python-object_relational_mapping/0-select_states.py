856ecb1d94170a951c147dc5f287696c14129543
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-17 08:22:29 +0000

    removed blank lines

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,17 +11,13 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], charset="utf8")
+                         user=argv[1], passwd=argv[2],
+                         db=argv[3], charset="utf8")
     cur = db.cursor()
-
     query = "SELECT * FROM states ORDERED BY states.id ASC"
     cur.execute(query)
-
     rows = cur.fetchall()
-
     for row in rows:
         print(row)
-
     cur.close()
     db.close()
