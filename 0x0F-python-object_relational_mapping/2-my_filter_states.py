05179a0f0f8f53a45b2b11e48d6df0e9695164ab
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-18 11:58:42 +0000

    system arguments for mysql

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,19 +18,24 @@ if __name__ == "__main__":
     state_name = sys.argv[4]
 
     # Connect to MySQL server
-    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(
+        host='localhost', 
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
     cur = db.cursor()
 
     # Create and execute the SQL query
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(state_name)
-    cur.execute(query)
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY states.id ASC".format(state_name))
 
     # Fetch and display the results
-    results = cur.fetchall()
-    for row in results:
+    selected = cur.fetchall()
+    for row in selected:
         print(row)
 
     # Close the cursor and database connection
     cur.close()
     db.close()
-
