c84c3862e7719a182751a45e82dd35605cad259c
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-18 08:18:15 +0000

    Script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where  matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where `name` matches the argument
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQL.connect(host="localhost", port=3306,
+                       user=sys.argv[1], passwd=sys.argv[2],
+                       db=sys.argv[3])
+    mycursor = cursor()
+    mycursor.execute("SELECT * FROM states WHERE name
+                     LIKE BINARY '{}'\\ ORDER BY id
+                     ASC".format(sys.argv[4]))
+    for row in mycursor.fetchall():
+        print(row)
+
+    mycursor.close()
+    db.close()
