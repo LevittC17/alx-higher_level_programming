232f7b8cd840006a3a6c552e3174fb2c7d906742
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-18 12:17:39 +0000

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -24,7 +24,8 @@ if __name__ == "__main__":
     mycur.execute(
             "SELECT * FROM states WHERE\
                     BINARY name='{}' ORDER BY id ASC".format(sys.argv[4]))
-    for row in mycur.fetchall():
+    query = mycur.fetchall()
+    for row in query:
         print(row)
     cur.close()
     conn.close()
