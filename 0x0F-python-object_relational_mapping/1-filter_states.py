9956a7b1d8c38a0052d50e51cf077cdaed43348a
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-18 08:28:21 +0000

    removed comment-like quotes

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id ASC""")
+    cur.execute("SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id ASC")
     for row in cur.fetchall():
         print(row)
 
