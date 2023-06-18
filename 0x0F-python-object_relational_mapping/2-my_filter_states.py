ad46e50ec758cc83e695b0a6bf50782ca3aa3f20
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-18 12:11:29 +0000

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,44 +1,33 @@
 #!/usr/bin/python3
 
-"""
-script that takes in an argument and displays
-all values in the states table of hbtn_0e_0_usa
-where name matches the argument.
+""" This module takes an argument and displays all values states
+    database hbtn_0e_0_usa WHERE name matches the argument.
 """
 
 
-import sys
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    # Retrieve command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    """
+        Function containing code to select the state provided
+        in the argument.
+    """
+
+    # Create a database connection
+    mydb = MySQLdb.connect(
+                host="localhost", port=3306, user=sys.argv[1],
+                passwd=sys.argv[2], db=sys.argv[3], charset="utf8mb4"
+            )
+    mycur = mydb.cursor()
+    # Select states
     state_name = sys.argv[4]
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-        charset="utf8"
-    )
-    cur = db.cursor()
-
-    # Create and execute the SQL query
-    cur.execute("SELECT *
-                FROM states WHERE BINARY
-                name='{}' ORDER BY states.id ASC".format(state_name)
-                )
-
-    # Fetch and display the results
-    selected = cur.fetchall()
-    for row in selected:
+    mycur.execute(
+            "SELECT * FROM states WHERE\
+                    BINARY name='{}' ORDER BY id ASC".format(state_name))
+    query_rows = mycur.fetchall()
+    for row in query_rows:
         print(row)
-
-    # Close the cursor and database connection
     cur.close()
-    db.close()
+    conn.close()
---
+++ 0x0F-python-object_relational_mapping/filterPY.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Retrieve command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
+    cur = db.cursor()
+
+    # Create and execute the SQL query
+    cur.execute("SELECT *
+                FROM states WHERE BINARY
+                name='{}' ORDER BY states.id ASC".format(state_name)
+                )
+
+    # Fetch and display the results
+    selected = cur.fetchall()
+    for row in selected:
+        print(row)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
