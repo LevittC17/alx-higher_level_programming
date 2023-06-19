fca57a26eda64f4f42313f021d1a4fc85c4b7c71
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-19 10:17:56 +0000

    in ascending order

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     cur.execute("SELECT cities.id,\
                 cities.name, states.name FROM cities\
                 INNER JOIN states ON\
-                cities.state_id=states.id ORDER BY cities.id")
+                cities.state_id=states.id ORDER BY cities.id ASC")
 
     mycities = mycursor.fetchall()
     for data in mycities:
