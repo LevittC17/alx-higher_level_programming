4ab62dd30dbc1d00c1dc2a2a338c2a0094f2330d
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-19 10:30:37 +0000

    INNER JOIN

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     mycursor.execute("SELECT cities.id,\
                 cities.name, states.name FROM cities\
-                JOIN states ON\
+                INNER JOIN states ON\
                 cities.state_id=states.id ORDER BY cities.id ASC")
 
     mycities = mycursor.fetchall()
