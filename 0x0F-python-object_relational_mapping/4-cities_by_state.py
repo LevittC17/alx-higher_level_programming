be55a116659a4ede66422dd2650b66d0beff59ed
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-19 10:16:45 +0000

    Listing all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,10 +15,10 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], charset='utf8mb4')
     mycursor = mydb.cursor()
 
-    mycursor.execute("SELECT cities.id, cities.name, states.name\
-                     FROM cities\
-                     LEFT JOIN states ON cities.state_id=states.id\
-                     ORDER BY cities.id ASC")
+    cur.execute("SELECT cities.id,\
+                cities.name, states.name FROM cities\
+                INNER JOIN states ON\
+                cities.state_id=states.id ORDER BY cities.id")
 
     mycities = mycursor.fetchall()
     for data in mycities:
