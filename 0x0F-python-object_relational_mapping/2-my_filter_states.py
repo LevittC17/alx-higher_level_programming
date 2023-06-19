654cda2ab701b8313a14853cea62010956a0d648
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-19 07:13:44 +0000

    character set

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     # Create a database connection
     mydb = MySQLdb.connect(
                 host="localhost", port=3306, user=sys.argv[1],
-                passwd=sys.argv[2], db=sys.argv[3], charset="utf8"
+                passwd=sys.argv[2], db=sys.argv[3], charset="utf8mb4"
             )
     mycur = mydb.cursor()
     state_name = sys.argv[4]
