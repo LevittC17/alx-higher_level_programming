a4f115a2d6e1d567ab4ae3b031f5c4b3d0c50831
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-23 07:18:19 +0000

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
-
-"""
-script that takes in the name of a state
-as an argument and lists all cities of
-that state, using the database hbtn_0e_4_usa
-"""
+"""takes in the name of a state as an argument and lists all cities of that
+ state, using the database hbtn_0e_4_usa"""
 
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    # connect to MySQL database
-    mydb = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3])
-    mycursor = db.cursor()
 
-    mycursor.execute("SELECT cities.id, cities.name, states.name \
-    FROM cities JOIN states ON cities.state_id = states.id \
-    WHERE states.name = '{}';".format(sys.argv[4]))
+    mydb = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    data = mycursor.fetchall()
-    print(', '.join([x[0] for x in data]))
-
-    mycursor.close()
-    mydb.close()
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT cities.name\
+                FROM cities LEFT JOIN states\
+                ON states.id = cities.state_id\
+                WHERE states.name = %s\
+                ORDER BY cities.id ASC", (sys.argv[4],))
+    mydata = mycursor.fetchall()
+    print(", ".join([data[0] for data in mydata]))
+    cur.close()
+    db.close()
