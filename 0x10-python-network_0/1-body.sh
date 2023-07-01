1e18ce226826b3edc007fd32c1c48d238151e01c
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-01 08:52:39 +0000

    display 200 response code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Display the body of the response 200 from the url
-#curl -sf -w "\n%{http_code}\n" "$1"
-curl -sL "$1" -X GET
+# Usage: get url, display 200 response code
+curl -sf -w "\n%{http_code}\n" "$1"
