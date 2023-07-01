c23336cd2123dd188e253e8426eda102074ab023
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-01 09:55:55 +0000

    send a DELETE request to a url

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a DELETE request to the URL passed, display body response
-curl -sSL -X DELETE "$1" | awk -v RS= '{$1=$1}1' | sed -n 's/.*\r\n\r\n\(.*\)/\1/p'
+curl -sSL -X DELETE "$1"
