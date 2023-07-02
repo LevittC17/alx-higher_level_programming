bc113f956153444b747593ca8497229755337cc8
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-02 16:18:29 +0000

    make a request to a server and respond with a message

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a request to 0.0.0.0:5000/catchme, server responds with a message
-curl -s -o /dev/null -w "%{http_code}\n" 0.0.0.0:5000/catch_me | grep -o "You got me!"
+curl -sL -X PUT -H "You got me!" 0.0.0.0:5000/catch_me
