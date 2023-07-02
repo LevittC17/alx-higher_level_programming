49756c9a58812dc09ce8e37f13038fc2474400b3
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-02 16:55:33 +0000

    find the peak element in a list of unsorted integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,47 +1,33 @@
 #!/usr/bin/python3
 
 """
-Finding a peak in a list of
-unsorted integers
+Find peak in a list of unsorted integers
+There might be more than one peak in the list
 """
 
 
 def find_peak(list_of_integers):
-    # Get the length of the list
-    n = len(list_of_integers)
+    """
+    Returns a peak in a list of unsorted integers.
+    Args: list_of_integers (list): unsorted
+    Return:peak element in the list, or None if empty.
+    """
 
-    # Base case: if the list is empty, there is no peak
-    if n == 0:
+    if not list_of_integers:  # Check if the list is empty
         return None
 
-    # Base case: if the list has only one element, it is a peak
-    if n == 1:
-        return list_of_integers[0]
+    left = 0
+    right = len(list_of_integers) - 1
 
-    # Base case: if the first element is a peak
-    if list_of_integers[0] >= list_of_integers[1]:
-        return list_of_integers[0]
-
-    # Base case: if the last element is a peak
-    if list_of_integers[n-1] >= list_of_integers[n-2]:
-        return list_of_integers[n-1]
-
-    # Perform binary search for a peak
-    left = 1
-    right = n - 2
-
-    while left <= right:
+    while left < right:
         mid = (left + right) // 2
-        mid_val = list_of_integers[mid]
 
         # Check if the middle element is a peak
-        if list_of_integers[mid-1] <= mid_val >= list_of_integers[mid+1]:
-            return mid_val
-        elif list_of_integers[mid-1] > mid_val:
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
             # Search in the left half
-            right = mid - 1
+            right = mid
         else:
             # Search in the right half
             left = mid + 1
 
-    return None
+    return list_of_integers[left]
