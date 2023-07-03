0a19efc7e40e9c3f24aaf2874820d6af60ac37b8
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-03 19:00:39 +0000

    update: sever response

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a request, server responds with a message
-curl -s -X POST -H "Referer: 0.0.0.0:5000/catch_me" 0.0.0.0:5000/catch_me
+curl -s 0.0.0.0:5000/catch_me --request POST --data "user_id=98" --header "Origin: HolbertonSchool"
