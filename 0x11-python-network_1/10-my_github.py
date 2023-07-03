18ca97e5e7a8819810f7ef657cf9156bfd534113
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-03 16:36:09 +0000

    Get id from GitHub credentials: username + personal access token

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+Get GitHub credentials, use the GitHub API
+to display the Id
+"""
+
+import requests
+import sys
+
+
+def main():
+    # Fetch the username and access token
+    username = sys.argv[1]
+    token = sys.argv[2]
+
+    # Fetch the GitHub API URL
+    url = 'https://api.github.com/user'
+
+    # Pass the personal access token as a header
+    headers = {
+        'Authorization': 'token {}'.format(token),
+        'Accept': 'application/vnd.github.v3+json'
+    }
+
+    # Retreive json data and store it in data variable
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    # Print specific fields from the response
+    if 'id' in data:
+        print(data['id'])
+    else:
+        print("Unable to retrieve user ID")
+
+
+if __name__ == "__main__":
+    main()
