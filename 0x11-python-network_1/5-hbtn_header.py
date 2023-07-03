12e57062ada0d2710c2990990fd32171f5048eae
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-03 14:33:39 +0000

    display the variable X-Request-Id with the requests package

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+send a request to a URL and dispay the
+variable X-Request_id
+use requests package
+"""
+
+
+import requests
+import sys
+
+
+def main():
+    # fetch the URL
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    request_id = response.headers.get('X-Request-Id')
+
+    print(request_id)
+
+if __name__ == "__main__":
+    main()
