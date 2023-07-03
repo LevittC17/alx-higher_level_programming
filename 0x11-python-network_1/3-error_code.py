301c1043f72e247ad21ea2a44321fc99826446ba
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-03 18:18:16 +0000

    update: Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -19,8 +19,7 @@ def main():
     # Using a try except block
     try:
         with urllib.request.urlopen(request) as response:
-            body = response.read().encode('utf-8')
-            print('Body response:')
+            body = response.read().decode('utf-8')
             print(body)
     except urllib.error.HTTPError as e:
         print(f'Error code: {e.code}')
