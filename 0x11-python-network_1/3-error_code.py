0b2de5ef160d03b8f2342d2ed743ccf9705b8fec
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-03 14:10:05 +0000

    urllib.error.HTTPError

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+send a request to a URL, display response body
+managing urllib.error.HTTPError
+"""
+
+
+import urllib.request
+import urllib.error
+import sys
+
+
+def main():
+    # Get URL from sys argument
+    url = sys.argv[1]
+
+    # Using a try except block
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().encode('utf-8')
+            print('Body response:')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print('Error code:', e.code)
+
+if __name__ == "__main__":
+    main()
