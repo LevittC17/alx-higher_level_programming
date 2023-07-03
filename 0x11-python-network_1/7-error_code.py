3e5af960e6dd61ec9bc2405b6bcec27b59853345
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-03 14:55:14 +0000

    if HTTP status code is > 400, used requests package

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+send a request to a URL and display response body
+if HTTP status code is > 400, print Error code
+use requests package
+"""
+
+
+import requests
+import sys
+
+
+def main():
+    # fetch the url
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    body = response.text
+
+    # Check if status_code is >= 400
+    if response.status_code >= 400:
+        print('Error code:', response.status_code)
+    else:
+        print(body)
