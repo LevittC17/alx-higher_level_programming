af207874fd277986a1c2d2851fe79a2698da1b6d
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-03 18:39:44 +0000

    Catch me if you can! : Task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a request, server responds with a message
-curl -s -X POST http://0.0.0.0:5000/catch_me -o /dev/null -w "You got me!"
+curl -sL -X PUT http://0.0.0.0:5000/catch_me -o /dev/null -w "You got me!"
