1f9a34460dea73033de90736547288a5c52e5eb3
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-03 14:46:33 +0000

    POST an email with requests package

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Get URL and email address, send a POST request
+with the email as parameter
+display body response
+"""
+
+
+import requests
+import sys
+
+
+def main():
+    # get URL and email
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {'email': email}
+    response = requests.post(url, data=data)
+
+    print('Your email is:', email)
+    # print('Body response')
+    # print(response.text)
+
+
+if __name__ == "__main__":
+    main()
