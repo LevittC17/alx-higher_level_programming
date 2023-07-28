b733c9b89834956cb5b6e049c8db238cc847ec01
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-28 21:49:39 +0000

    make a request to a server: respond with a message

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a request, server responds with a message
-curl -s 0.0.0.0:5000/catch_me --request POST --data "user_id=98" --header "Origin: HolbertonSchool"
+curl -s -X PUT -H "Origin: HolbertonSchool" --data "user_id=98" "0.0.0.0:5000/catch_me
